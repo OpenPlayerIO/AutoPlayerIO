493ea39b30d033ff7e7a424b198dc2534d22a348
atillabyte <atillabyte@null.net>
2020-06-05 00:31:54 -0400

    Update Connection.cs
    add missing summaries

--- src/Connections/Connection.cs
+++ src/Connections/Connection.cs
@@ -2,7 +2,14 @@
 {
     public class Connection
     {
+        /// <summary>
+        /// The name of the connection.
+        /// </summary>
         public string Name { get; }
+
+        /// <summary>
+        /// The description of the connection.
+        /// </summary>
         public string Description { get; }
 
         internal Connection(string name, string description)
